ac012959d00e97b64ffec7212457cd5a38fbdc1a
t21389 <hitenpatel0775@gmail.com>
2023-04-26 12:00:09 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}    */
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 const nextConfig = (phase) => {
